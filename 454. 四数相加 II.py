b5efedf782329e71f9248fe56cb60ed7c5a9ce2d
Zhanghonghua <457285383@qq.com>
2018-10-30 09:37:24 +0800

    Create 454. 四数相加 II.py

---
+++ 454. 四数相加 II.py
@@ -0,0 +1,46 @@
+'''
+给定四个包含整数的数组列表 A , B , C , D ,计算有多少个元组 (i, j, k, l) ，使得 A[i] + B[j] + C[k] + D[l] = 0。
+
+为了使问题简单化，所有的 A, B, C, D 具有相同的长度 N，且 0 ≤ N ≤ 500 。所有整数的范围在 -228 到 228 - 1 之间，最终结果不会超过 231 - 1 。
+
+例如:
+
+输入:
+A = [ 1, 2]
+B = [-2,-1]
+C = [-1, 2]
+D = [ 0, 2]
+
+输出:
+2
+
+解释:
+两个元组如下:
+1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
+2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0
+'''
+# 时间复杂度：O(n^2)
+# 空间复杂度：O(n)
+class Solution:
+    def fourSumCount(self, A, B, C, D):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :type C: List[int]
+        :type D: List[int]
+        :rtype: int
+        """
+        dict1={}
+        res=0
+        for i in range(len(A)):
+            for j in range(len(B)):
+                key=A[i]+B[j]
+                dict1[key]=dict1.get(key,0)+1
+        
+        for k in range(len(C)):
+            for l in range(len(D)):
+                key=-(C[k]+D[l])
+                res+=dict1.get(key,0)
+        return res
+        
+        
